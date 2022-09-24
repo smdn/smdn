be00324f6e473ba0b59bf42d73cadb905dd4abc5
smdn <smdn@smdn.jp>
2022-09-25 03:03:24 +0900

    add deployment script

---
+++ etc/home/smdn/deploy-dotfiles.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+basedir=$(cd $(dirname $0) && pwd)
+backup_suffix=.original~
+COPY="rsync --verbose --backup --suffix=${backup_suffix} --checksum --perms --times"
+
+# copy dotfiles to ${HOME}
+${COPY} \
+  ${basedir}/.vimrc \
+  ${basedir}/.bash_aliases \
+  ${basedir}/.bash_prompt \
+  ${basedir}/.bash_envname \
+  ${HOME}
+
+# apply patch to ~/.bashrc
+PATCH_FILE_BASHRC=${basedir}/.bashrc.ubuntu-desktop.patch
+PATCH_ARGS_BASHRC="--directory=${HOME} --forward --backup-if-mismatch --suffix=${backup_suffix}"
+
+patch --dry-run ${PATCH_ARGS_BASHRC} < ${PATCH_FILE_BASHRC}
+
+if [ $? -eq 0 ]; then
+  patch ${PATCH_ARGS_BASHRC} < ${PATCH_FILE_BASHRC}
+fi
+
+# deploy git-prompt.sh
+curl -L https://raw.githubusercontent.com/git/git/e03acd0d4ad75115f454041fac3300ae796f108f/contrib/completion/git-prompt.sh --output ${HOME}/.git-prompt.sh
+
+# copy dotfiles to ${HOME}/.config
+if [ $EUID -ne 0 ]; then
+  destdir=${HOME}/.config
+  mkdir -p ${destdir}
+  ${COPY} \
+    ${basedir}/.config/user-dirs.conf \
+    ${basedir}/.config/user-dirs.dirs \
+    ${basedir}/.config/user-dirs.locale \
+    ${destdir}
+fi
+
