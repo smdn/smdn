e9a8251def15825e84764e5505a4235f9ab11c04
smdn <smdn@smdn.jp>
2025-07-22 20:14:26 +0900

    add firefox user.js

---
+++ etc/home/smdn/confs/firefox/user.js
@@ -0,0 +1,18 @@
+user_pref("accessibility.browsewithcaret", true); // キャレットブラウズモードを有効にする
+
+user_pref("browser.search.openintab", true); // 検索結果を新しいタブで開く
+user_pref("browser.search.widget.inNavBar", true); // 検索バーをツールバーに表示する
+user_pref("browser.startup.page", 3); // 起動時に前回のウィンドウとタブを開く
+user_pref("browser.tabs.loadInBackground", false); // リンクを新しいタブで開いたとき、すぐにそのタブに切り替える
+user_pref("browser.tabs.loadBookmarksInTabs", true); // ブックマークを新しいタブで開く
+user_pref("browser.tabs.closeWindowWithLastTab", false); // 最後のタブを閉じたあともウィンドウを開いたままにする
+user_pref("browser.tabs.warnOnClose", false); // 複数のタブを閉じるときにダイアログを表示しない
+
+user_pref("general.autoScroll", false); // 自動スクロール機能を使用しない
+user_pref("general.smoothScroll", false); // スムーススクロールを無効にする
+
+user_pref("intl.accept_languages", "ja,en"); // 言語の優先順位(Accept-Language)を設定する
+
+user_pref("layout.spellcheckDefault", 0); // 自動スペルチェック機能を使用しない
+
+user_pref("signon.rememberSignons", false); // ウェブサイトのログイン情報とパスワードを保存しない
